ec0e036e15be1eebb05cdbe80d43d0e7b08b3220
DarlanP17 <darlan.perdomo@unah.hn>
2024-07-17 11:21:37 -0600

    Creacion de los widgets inputs personalizados

---
+++ app_login/lib/widgets/custom_inputs.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class CustomInputs extends StatelessWidget {
+  const CustomInputs({
+    super.key,
+    required this.controller,
+    required this.validator,
+    required this.teclado,
+    required this.length,
+    required this.hint,
+    required this.nombrelabel,
+    required this.icono
+    });
+
+  final TextEditingController controller;
+  final String? Function(String?)? validator; 
+  final TextInputType teclado;
+  final int? length;
+  final String? hint;
+  final String nombrelabel;
+  final IconData? icono;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      validator: validator,
+      keyboardType: teclado,
+      maxLength: length,
+      maxLines: 1,
+      decoration: InputDecoration(
+        hintText: hint,
+        label: Text(nombrelabel),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        prefixIcon: Icon(icono),
+      ),
+    );
+  }
+}
---
+++ app_login/lib/widgets/password_input.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class PasswordInput extends StatefulWidget {
+  const PasswordInput({
+    super.key,
+    required this.nombrelabel, 
+    required this.hint, 
+    required this.controller,
+    });
+
+final String nombrelabel;
+  final String hint;
+  final TextEditingController controller;
+
+  @override
+  State<PasswordInput> createState() => _PasswordInputState();
+}
+
+class _PasswordInputState extends State<PasswordInput> {
+
+  bool contra = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: widget.controller,
+      obscureText: contra,
+      keyboardType: TextInputType.text,
+      maxLength: 30,
+      maxLines: 1,
+      decoration: InputDecoration(
+        hintText: widget.hint,
+        label: Text(widget.nombrelabel),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        prefixIcon: Icon(Icons.password),
+        suffixIcon: IconButton(
+                    icon: Icon(
+                        contra ? Icons.visibility : Icons.visibility_off),
+                    onPressed: () {
+                      setState(() {
+                        contra = !contra;
+                      });
+                    }),
+      ),
+    );
+  }
+}
